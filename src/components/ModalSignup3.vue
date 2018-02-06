8305375a6598dc7e087c8621cdc90ed50dab1fd7
groovypark <soobin950@gmail.com>
2018-02-06 17:54:05 +0900

    Update style

--- src/components/ModalSignup3.vue
+++ src/components/ModalSignup3.vue
@@ -1,19 +1,23 @@
 <template>
-  <div class="ModalSignup1">
+  <div class="ModalSignup3">
     <img src="static/assets/signImage/signup1/welcome.png"
          alt="WELCOME! 환영합니다. 핸디즈 회원가입">
     <img class="agreement-header"
          src="static/assets/signImage/signup3/order3.png"
          alt="1.이용 약관 동의">
     <hr>
-    <div style="text-align:center;">
+    <div style="text-align:center; margin-top:52px;">
       OOO 호스트님,<br/>
       회원 가입이 완료되었습니다.<br/>
       앞으로 핸디즈 서비스 이용이 가능합니다.<br/>
-      <img src="static/assets/signImage/signup3/clap.png"
-           alt="박수">
+      <div style="padding: 66px 0 77px 0">
+        <img src="static/assets/signImage/signup3/clap.png"
+             alt="박수">
+      </div>
+      <a href="/">
       <img src="static/assets/signImage/signup3/btnHome.png"
            alt="홈으로">
+      </a>
     </div>
   </div>
 </template>
