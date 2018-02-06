8d598b811ca587850afbbd13eb977771e7b0c7f7
groovypark <soobin950@gmail.com>
2018-02-06 16:48:52 +0900

    Update form group

--- src/components/ModalSignup2.vue
+++ src/components/ModalSignup2.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="ModalSignup2">
+  <div class="ModalSignup2 font-notosans" style="font-size:15px;">
     <img src="static/assets/signImage/signup1/welcome.png"
          alt="WELCOME! 환영합니다. 핸디즈 회원가입">
     <img class="agreement-header"
@@ -14,7 +14,7 @@
                alt="이름">
         </label>
           <input class="input" type="text" name="name">
-          <div class="err-text font-notosans">필수 항목입니다</div>
+          <div class="err-text font-notosans">필수 항목입니다.</div>
       </b-form-group>
       <!-- 전화번호 -->
       <b-form-group>
@@ -22,25 +22,27 @@
           <img src="static/assets/signImage/signup2/phone.png"
                alt="전화번호">
         </label>
-          <input class="input" type="number" name="phoneNumber" placeholder="휴대전화 번호를 입력해 주세요">
-          <div class="err-text font-notosans">필수 항목입니다</div>
+          <input class="input" type="tel" name="phoneNumber" placeholder="휴대전화 번호를 입력해 주세요.">
+          <div class="err-text font-notosans">필수 항목입니다.</div>
       </b-form-group>
       <!-- 전화번호 인증 -->
       <b-form-group>
-        <label for="phoneNumberCheck" style="width:115px;">
+        <label for="phoneNumberCheck">
           <img src="static/assets/signImage/signup2/phoneNum.png"
             alt="전화번호 인증">
         </label>
-        <input class="input" type="number" name="phoneNumberCheck" placeholder="문자로 받은 인증번호 6자리 입력해 주세요">
-        <a>
-          <img src="static/assets/signImage/signup2/phoneNumOff.png"
-            alt="인증번호 받기">
-        </a>
-        <a>
-          <img src="static/assets/signImage/signup2/resendOff.png"
-            alt="재발송">
-        </a>
-          <div class="err-text font-notosans">인증코드를 발송하였습니다</div>
+        <span style="float:right;">
+          <a style="margin-right:6px;">
+            <img src="static/assets/signImage/signup2/phoneNumOff.png"
+              alt="인증번호 받기">
+          </a>
+          <a>
+            <img src="static/assets/signImage/signup2/resendOff.png"
+              alt="재발송">
+          </a>
+        </span>
+        <input class="input" style="width:191px; margin-right:8px;" type="text" name="phoneNumberCheck" placeholder="문자로 받은 인증번호 6자리 입력해 주세요.">
+        <div class="err-text font-notosans">인증코드를 발송하였습니다.</div>
       </b-form-group>
       <!-- 아이디 -->
       <b-form-group>
@@ -48,8 +50,8 @@
           <img src="static/assets/signImage/signup2/id.png"
             alt="아이디">
         </label>
-        <input class="input" type="email" name="id" placeholder="E-mail 주소를 입력해 주세요">
-        <div class="err-text font-notosans">정확한 E-mail 주소를 입력해 주세요</div>
+        <input class="input" type="email" name="id" placeholder="E-mail 주소를 입력해 주세요.">
+        <div class="err-text font-notosans">정확한 E-mail 주소를 입력해 주세요.</div>
       </b-form-group>
       <!-- 비밀번호 -->
       <b-form-group>
@@ -58,7 +60,7 @@
             alt="비밀번호">
         </label>
         <input class="input" type="text" name="password" placeholder="">
-        <div class="err-text font-notosans">문자와 숫자 조합으로 8자 이상 입력해 주세요</div>
+        <div class="err-text font-notosans">문자와 숫자 조합으로 8자 이상 입력해 주세요.</div>
       </b-form-group>
       <!-- 비밀번호 확인 -->
       <b-form-group>
@@ -75,52 +77,69 @@
           <img src="static/assets/signImage/signup2/gender.png"
             alt="성별">
         </label>
-        <input type="radio" name="gender"> 남성
-        <input type="radio" name="gender"> 여성
+        <span v-for="gender in genders" :key="gender.id">
+          <input class="radio-gender" name="gender" type="radio" v-bind:value="gender.value"> {{ gender.text }}
+        </span>
       </b-form-group>
       <!-- 연령대 -->
       <b-form-group>
-        <label for="age">
+        <label for="age" style="float:left;">
           <img src="static/assets/signImage/signup2/age.png"
             alt="연령대">
         </label>
-        <input type="radio" name="10"> 10대 이하
-        <input type="radio" name="20"> 20대
-        <input type="radio" name="30"> 30대
-        <input type="radio" name="40"> 40대
-        <input type="radio" name="50"> 50대
-        <input type="radio" name="60"> 60대 이상
+        <table style="margin-left:107px;">
+            <tr>
+              <td style="width:121px; height:46px; vertical-align:top;" v-for="age in ages1" :key="age.id">
+                <input  type="radio" name="age" v-bind:value="age.value"> {{ age.text }}
+              </td>
+            </tr>
+            <tr>
+              <td v-for="age in ages2" :key="age.id">
+                <input  type="radio" name="age" v-bind:value="age.value"> {{ age.text }}
+              </td>
+            </tr>
+        </table>
       </b-form-group>
       <!-- 호스팅 지역 -->
-      <b-form-group>
-        <label for="local">
+      <form style="margin-top: 23px; margin-bottom:30px">
+        <label for="local" style="margin-right:32px;">
           <img src="static/assets/signImage/signup2/local.png"
             alt="호스팅 지역">
         </label>
-        <input type="radio" name="seoul"> 서울
-        <input type="radio" name="busan"> 부산
-        <input type="radio" name="jeju"> 제주
-        <input type="radio" name="etcl"> 그 외
-      </b-form-group>
+        <span style="margin-right:73px;" v-for="local in locals" :key="local.id">
+          <input type="radio" name="local" v-bind:value="local.value"> {{ local.text }}
+        </span>
+      </form>
     </b-form>
-
-    <!-- 버튼 -->
-    <div class="form-group">
-      <a style="margin-left:103px;">
-        <img src="static/assets/signImage/signup2/btnBefore.png"
-            alt="이전 단계">
-      </a>
-      <a style="float:right; margin-right:103px;">
-        <img src="static/assets/signImage/signup2/btnNext.png"
-            alt="다음 단계">
-      </a>
-    </div>
-
   </div>
 </template>
 <script>
 export default {
-  name: 'ModalSignup1'
+  name: 'ModalSignup1',
+  data: function () {
+    return {
+      genders: [
+        { text: '남성', value: 'male' },
+        { text: '여성', value: 'femal' }
+      ],
+      ages1: [
+        { text: '10대 이하', value: 10 },
+        { text: '20대', value: 20 },
+        { text: '30대', value: 30 }
+      ],
+      ages2: [
+        { text: '40대', value: 40 },
+        { text: '50대', value: 50 },
+        { text: '60대 이상', value: 60 }
+      ],
+      locals: [
+        { text: '서울', value: 'seoul' },
+        { text: '부산', value: 'busan' },
+        { text: '제주', value: 'jeju' },
+        { text: '그 외', value: 'etc' }
+      ]
+    }
+  }
 }
 </script>
 <style scoped>
@@ -140,6 +159,17 @@ hr {
 	letter-spacing: -0.5px;
 	text-align: left;
 	color: #e60012;
-  margin-left: 135px;
+  margin-left: 110px;
+}
+.radio-gender {
+  margin-left: 75px;
+}
+.radio-age {
+  float: right;
+  margin-right: 65px;
+}
+
+label.form-check-label {
+  margin-left: 40px;
 }
 </style>
