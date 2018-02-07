369086c139e6eed245771754a74a9334c03e6630
groovypark <soobin950@gmail.com>
2018-02-07 12:19:01 +0900

    Update button

--- src/components/FindIdPw.vue
+++ src/components/FindIdPw.vue
@@ -14,7 +14,7 @@
       <input class="input" type="text" name="name" placeholder=" 휴대전화 번호를 입력하세요.">
     </div>
     <div class="err-text">회원 가입이 안된 회원입니다.</div>
-    <img class="btnConfirm" src="static/assets/signImage/checkId/btnConfirm.png" alt="확인">
+    <img class="btnConfirm" src="static/assets/signImage/btnConfirm.png" alt="확인">
     <hr class="hr-margin">
     <img src="static/assets/signImage/findIdPw/findPw.png" alt="비밀번호 찾기"><br/>
     <img class="explanation" src="static/assets/signImage/findIdPw/findPwText.png" alt="본인 인증으로 비밀번호를 변경하세요.">
@@ -23,10 +23,6 @@
       <input class="input" type="text" name="id" placeholder=" E-mail 주소를입력하세요.">
     </div>
     <div class="err-text">가입되지 않은 메일주소 입니다. </div>
-    <div class="btn">
-      <img src="static/assets/signImage/findIdPw/btnEmail.png" alt="이메일 인증">
-      <img src="static/assets/signImage/findIdPw/btnPhone.png" alt="휴대전화 인증">
-    </div>
   </div>
 </template>
 <script>
@@ -60,10 +56,5 @@ hr {
 }
 .hr-margin {
   margin: 91px 0 37px 0;
-}
-div.btn {
-  float: right;
-  padding: 0;
-  margin: 44px 0 27px 0;
 }
 </style>
