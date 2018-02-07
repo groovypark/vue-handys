9658bf7fb4efdd04070f8e44e1c72fc4d9381484
groovypark <soobin950@gmail.com>
2018-02-07 11:18:39 +0900

    Create Phone.vue

---
+++ src/components/Phone.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="phone">
+    <img src="static/assets/signImage/phone/title.png" alt="인증번호 입력">
+    <hr>
+  </div>  
+</template>
+<script>
+export default {
+}
+</script>
+<style scoped>
+hr {
+  width:531px;
+  height:1px;
+  background: #ccc;
+}
+</style>
